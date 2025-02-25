1b93db5e1550c555eb23d6bdcf071512e6333ffc
Sonam Kumari <aspritualgirl@gmail.com>
2025-02-25 22:12:28 +0530

    Program2

--- Section_1_Introduction/Program1.java
+++ Section_1_Introduction/Program1.java
@@ -2,6 +2,7 @@ package Section_1_Introduction;
 
 public class Program1 {public static void main(String[] args) {
     System.out.println("Hello world");
+    System.out.println("Sonam");
 }
     
 }
